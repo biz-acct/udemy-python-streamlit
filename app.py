f815883383198672ca0fa32aeb20c81958b50816
biz-acct <bizacct2507@gmail.com>
2025-03-17 22:57:17 +0900

    1st commit

---
+++ app.py
@@ -0,0 +1,202 @@
+import pandas as pd
+import streamlit as st
+
+import datetime
+
+st.title("Hello World")
+st.title("Hello World :sunglasses:")
+
+st.write("Hello World")
+st.write("Hello :blue[World]")
+
+st.write(
+    pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40]
+        }
+    )
+)
+
+st.link_button("Click Here", "https://docs.streamlit.io/develop/api-reference")
+
+st.header("Hello World", divider="rainbow")
+
+code = """
+    print("hello")
+"""
+st.code(code, language="python")
+
+agree = st.checkbox("I agree")
+if agree:
+    st.write("Okay!")
+
+options = st.multiselect(
+    "好きな色はなんですか？",
+    ["赤", "緑", "青", "黄"]
+)
+st.write("あなたが選んだ色：", options)
+
+options = st.radio(
+    "好きな色はなんですか？",
+    ["赤", "緑", "青", "黄"]
+)
+st.write("あなたが選んだ色：", options)
+
+# 修正できるデータフレーム
+df = pd.DataFrame(
+    [
+        {"colors": "赤", "rating": 4},
+        {"colors": "緑", "rating": 5},
+        {"colors": "青", "rating": 3},
+    ]
+)
+edited_df = st.data_editor(df)
+st.write(edited_df.loc[edited_df["rating"].idxmax()]["colors"])
+
+# 修正できるデータフレーム②
+df = pd.DataFrame(
+    [
+        {"colors": "赤", "rating": 4, "mark": True},
+        {"colors": "緑", "rating": 5, "mark": True},
+        {"colors": "青", "rating": 3, "mark": True},
+    ]
+)
+edited_df = st.data_editor(df)
+edited_df = edited_df[edited_df["mark"] == True]
+st.write(edited_df.loc[edited_df["rating"].idxmax()]["colors"])
+
+# ダウンロードボタン
+csv = edited_df.to_csv().encode("utf-8")
+
+st.download_button(
+    label="CSVダウンロード",
+    data=csv,
+    file_name="sample_df.csv",
+    mime="text/csv"
+)
+
+# プログレスバー表示
+df = pd.DataFrame(
+    {
+        "sales": [20, 55, 100, 80],
+        "progress_sales": [20, 55, 100, 80]
+    }
+)
+
+st.data_editor(
+    df,
+    column_config={
+        "progress_sales": st.column_config.ProgressColumn(
+            min_value=0,
+            max_value=100
+        )
+    }
+)
+
+# 時系列バー表示
+df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 30, 60, 80, 100],
+            [3, 50, 0, 80, 40, 30, 100]
+        ]
+    }
+)
+
+st.data_editor(df)
+
+st.data_editor(
+    df,
+    column_config={
+        "sales": st.column_config.BarChartColumn(
+            y_min=0,
+            y_max=100
+        )
+    }
+)
+
+# 時系列折れ線グラフ
+st.data_editor(
+    df,
+    column_config={
+        "sales": st.column_config.LineChartColumn(
+            y_min=0,
+            y_max=100
+        )
+    }
+)
+
+# スライダー
+age = st.slider("あなたは何歳ですか？", 0, 130, 40)
+st.write(f"私は{age}歳です")
+
+# 日付選択
+date = st.date_input("あなたが生まれたのはいつですか？", datetime.date(2000, 1, 1))
+st.write(f"私は{date}に生まれました")
+
+# ユーザーのフリー入力
+text = st.text_input("入力してください", "xxxxxxx")
+st.write(text)
+
+# カラムを分ける
+col1, col2 = st.columns(2)
+
+with col1:
+    st.title("Column1")
+    st.write("これはカラムの1です")
+
+with col2:
+    st.title("Column2")
+    st.write("これはカラムの2です")
+
+# タブを分ける
+tab1, tab2 = st.tabs(["tab1", "tab2"])
+
+with tab1:
+    st.title("Tab1")
+    st.write("これはタブの1です")
+
+with tab2:
+    st.title("Tab2")
+    st.write("これはタブの2です")
+
+# アコーディオン表示
+with st.expander("もっと詳しく見る"):
+    st.write("xxxxxxxxxx")
+
+# ポップアップ表示
+with st.popover("もっと詳しく見る"):
+    st.write("xxxxxxxxxx")
+
+# サイドバー
+with st.sidebar:
+    st.title("xxxxxxxxxx")
+    st.write("aaaaaaaaaaaaaaa")
+
+# notification
+agree = st.checkbox("同意しますか？")
+if agree:
+    st.toast("Thank you", icon="👍")
+    st.toast("Thank you2", icon="👍")
+    st.toast("Thank you3", icon="👍")
+
+# エフェクト
+birthday = st.checkbox("今日はあなたの誕生日ですか？")
+if birthday:
+    st.toast("誕生日おめでとう！", icon="👍")
+    st.balloons()
+
+# 複数ページ実装
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
+    st.page_link("app.py", label="Home", icon="🏠")
+with col2:
+    st.page_link("pages/page1.py", label="ページ1")
+with col3:
+    st.page_link("pages/page2.py", label="ページ2")
+with col4:
+    st.page_link("pages/page3.py", label="ページ3")
+with col5:
+    st.page_link("https://docs.streamlit.io/develop/api-reference", label="StreamlitのAPIドキュメント")
+    
